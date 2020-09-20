c253dea64f78f9a20f82d5cec07332f72cf46e63
ViniEugenio <vinicius.eugenio07@gmail.com>
2020-09-20 20:54:42 -0300

    First Commit

---
+++ EiSorteei/Data/Context.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EiSorteei.Data
+{
+    public class Context
+    {
+        public static EiSorteeiEntities _db;
+
+        public static EiSorteeiEntities GetContext()
+        {
+            return _db = new EiSorteeiEntities();
+        }
+    }
+}
